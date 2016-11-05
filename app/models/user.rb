4aa45381a3ca5ea0311f89c9660c1801dae82af2
Roberto Pando <roberto_pando@hotmail.com>
2016-11-05 21:58:17 +0100

    completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,41 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_save :downcase_email
   validates :name, presence: true, length: { maximum: 55 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104151706) do
+ActiveRecord::Schema.define(version: 20161105204704) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "author"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20161104151706) do
     t.boolean  "member",          default: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.string   "remember_digest"
+    t.boolean  "admin",           default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
